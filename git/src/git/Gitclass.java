47abcbb66872594960042a680de2e1c7fb60bd24
yaminisparity <49156749+yaminisparity@users.noreply.github.com>
2020-04-10 22:41:27 +0530

    gitcommit1

---
+++ git/src/git/Gitclass.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+    System.out.println("print line1");
+    System.out.println("print line2");
+    System.out.println("print line3");
+	}
+
+}
